3e5850ace972050ae32b74624ab061122bb4dce3
Alex <audut1197@gmail.com>
2023-03-17 06:47:06 +0100

    shipping collection added

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
     try {
       
       const params = {
+        shipping_address_collection: {allowed_countries: ['US', 'CA']},
         submit_type: 'pay',
         mode: 'payment',
         payment_method_types: ['card'], 
